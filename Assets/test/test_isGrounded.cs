c53133c9b9d65aec64bb565b8cade26024f27d77
KentaSugawara <zugatyatti@gmail.com>
2018-03-17 17:13:51 +0900

    no message

---
+++ Assets/test/test_isGrounded.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_isGrounded : MonoBehaviour {
+
+    [SerializeField]
+    private bool _isGrounded;
+    public bool isGrounded
+    {
+        get { return _isGrounded; }
+    }
+
+
+    private int ColliderCount;
+    private void OnTriggerEnter(Collider other)
+    {
+        _isGrounded = true;
+        ++ColliderCount;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        --ColliderCount;
+        if (ColliderCount <= 0)
+        {
+            _isGrounded = false;
+        }
+    }
+}
