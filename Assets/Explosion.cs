0a105a619f91a96b73043e4ee2d4eac3ca81021d
ogami <ganick0512@gmail.com>
2018-03-17 19:09:08 +0900

    no message

--- Assets/Explosion.cs
+++ Assets/Explosion.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Explosion : MonoBehaviour {
+    [SerializeField] float radius;
+
 
 	// Use this for initialization
 	void Start () {
         StartCoroutine(Delete());
+        var array = Physics.OverlapSphere(transform.position, radius, 1 << 10);
+        for(int i = 0; i < array.Length; i++)
+        {
+            var charscr = array[i].gameObject.GetComponent<test_Character>();
+            if (charscr != null)
+            {
+                charscr.Damage(1);
+            }
+        }
     }
 	IEnumerator Delete()
     {
