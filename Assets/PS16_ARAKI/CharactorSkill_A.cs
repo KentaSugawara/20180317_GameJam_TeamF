3758be1780a3c5a850d710b252ca667a63602530
PS16 ARAKI <shyu70613@gmail.com>
2018-03-18 11:08:24 +0900

    skill_playerArea

---
+++ Assets/PS16_ARAKI/CharactorSkill_A.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharactorSkill_A : SkillSuperClass
+{
+    [SerializeField]
+    private GameObject playerArea;
+    [SerializeField]
+    private float skillMag;//magnification
+
+    public override bool Use()
+    {
+        if (true)
+        {
+            playerArea.transform.localScale *= skillMag;
+            StartTimer();
+            return true;
+        }
+        return false;
+    }
+}
